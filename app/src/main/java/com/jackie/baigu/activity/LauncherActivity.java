d60f41145ea3b8225ea5302ae4544398f6480c13
jackie <971060378@qq.com>
2016-12-29 03:42:32 +0800

    完成框架的搭建！

--- app/src/main/java/com/jackie/baigu/activity/LauncherActivity.java
+++ app/src/main/java/com/jackie/baigu/activity/LauncherActivity.java
@@ -1,9 +1,13 @@
 package com.jackie.baigu.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.jackie.baigu.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Administrator on 2016/12/29.
@@ -14,5 +18,14 @@ public class LauncherActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        //延迟跳转下一个页面
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LauncherActivity.this,MainActivity.class));
+                LauncherActivity.this.finish();
+            }
+        },2000);
     }
 }
---
+++ app/src/main/java/com/jackie/baigu/fragment/OtherFragment.java
@@ -0,0 +1,35 @@
+package com.jackie.baigu.fragment;
+
+import android.graphics.Color;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+
+/**
+ * 作用：其他Fragment
+ */
+public class OtherFragment extends BaseFragment {
+
+
+    private static final String TAG = OtherFragment.class.getSimpleName();//"CommonFrameFragment"
+    private TextView textView;
+
+    @Override
+    protected View initView() {
+        Log.e(TAG,"其他Fragment页面被初始化了...");
+        textView = new TextView(mContext);
+        textView.setTextSize(20);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextColor(Color.RED);
+        return textView;
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+        Log.e(TAG, "其他Fragment数据被初始化了...");
+        textView.setText("其他页面");
+    }
+}
