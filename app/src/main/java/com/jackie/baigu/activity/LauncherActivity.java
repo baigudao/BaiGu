0cd0f6712888f486a0688e94113d423a57d6031d
jackie <971060378@qq.com>
2016-12-29 02:00:13 +0800

    准备工作的提交

---
+++ app/src/main/java/com/jackie/baigu/activity/LauncherActivity.java
@@ -0,0 +1,18 @@
+package com.jackie.baigu.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.jackie.baigu.R;
+
+/**
+ * Created by Administrator on 2016/12/29.
+ */
+public class LauncherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
